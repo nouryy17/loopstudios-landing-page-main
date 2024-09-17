926e11452099d7e99d98b4b8d0a2416d88950ed2
nouryy17 <dacinour16@gmail.com>
2024-09-17 15:48:04 +0200

    completed footer for desktop

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import HEADER from "./component/header"
 import MINECONTENT from "./component/mainContent"
 import CRETION from "./component/cretionPart"
+import FOOTER from "./component/footer"
 function App() {
   return (
     <div>
           <HEADER></HEADER>
           <MINECONTENT></MINECONTENT>
           <CRETION></CRETION>
+          <FOOTER></FOOTER>
     </div>
 
   );
