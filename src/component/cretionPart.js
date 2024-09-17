3e8562826b76c6098577d012288657cfa5598290
nouryy17 <dacinour16@gmail.com>
2024-09-17 18:10:37 +0200

    modulation

--- src/component/cretionPart.js
+++ src/component/cretionPart.js
@@ -8,6 +8,16 @@ import imgFaif from "../images/desktop/image-from-above.jpg"
 import imgSxe from "../images/desktop/image-pocket-borealis.jpg"
 import imgSven from "../images/desktop/image-curiosity.jpg"
 import imgEt from "../images/desktop/image-fisheye.jpg"
+import imgphoneOne from "../images/mobile/image-deep-earth.jpg"
+import imgphoneTow from "../images/mobile/image-night-arcade.jpg"
+import imgphoneThree from "../images/mobile/image-soccer-team.jpg"
+import imgphoneFour from "../images/mobile/image-grid.jpg"
+import imgphoneFaif from "../images/mobile/image-from-above.jpg"
+import imgphoneSxe from "../images/mobile/image-pocket-borealis.jpg"
+import imgphoneSven from "../images/mobile/image-curiosity.jpg"
+import imgphoneEt from "../images/mobile/image-fisheye.jpg"
+const w = window.innerWidth > 1000
+console.log(w)
 export default function () {
     return (
         <div className=" p-20">
@@ -15,15 +25,15 @@ export default function () {
                 <h1 className=" text-5xl uppercase font-extralight ">  Our creations </h1>
                 <button class=" mr-5 uppercase text-gray-700 px-10 py-1 border-x-2 border-y-2 border-gray-800"> see all</button>
             </div>
-            <div className=" grid lg:grid-cols-4 gap-2">
-                <CRTIONBOX src={imgOne} tital={"Deep earth"}></CRTIONBOX>
-                <CRTIONBOX src={imgTow} tital={" Night arcade"}></CRTIONBOX>
-                <CRTIONBOX src={imgThree} tital={"  Soccer team VR"}></CRTIONBOX>
-                <CRTIONBOX src={imgFour} tital={" The grid"}></CRTIONBOX>
-                <CRTIONBOX src={imgFaif} tital={"From up above VR "}></CRTIONBOX>
-                <CRTIONBOX src={imgSxe} tital={"Pocket borealis"}></CRTIONBOX>
-                <CRTIONBOX src={imgSven} tital={"The curiosity"}></CRTIONBOX>
-                <CRTIONBOX src={imgEt} tital={"Make it fisheye"}></CRTIONBOX>
+            <div className=" grid  lg:grid-cols-4 gap-2">
+                <CRTIONBOX src={w ? imgOne  : imgphoneOne } tital={"Deep earth"}></CRTIONBOX>
+                <CRTIONBOX src={w ? imgTow  : imgphoneTow } tital={" Night arcade"}></CRTIONBOX>
+                <CRTIONBOX src={w ? imgThree  : imgphoneThree } tital={"  Soccer team VR"}></CRTIONBOX>
+                <CRTIONBOX src={w ? imgFour  : imgphoneFour } tital={" The grid"}></CRTIONBOX>
+                <CRTIONBOX src={w ? imgFaif  : imgphoneFaif } tital={"From up above VR "}></CRTIONBOX>
+                <CRTIONBOX src={w ? imgSxe  : imgphoneSxe } tital={"Pocket borealis"}></CRTIONBOX>
+                <CRTIONBOX src={w ? imgSven  : imgphoneSven } tital={"The curiosity"}></CRTIONBOX>
+                <CRTIONBOX src={w ? imgEt  : imgphoneEt } tital={"Make it fisheye"}></CRTIONBOX>
             </div>
 
         </div>
--- src/component/crtionBox.js
+++ src/component/crtionBox.js
@@ -1,6 +1,6 @@
 export default function ({src,tital}) {
     return(
-        <div className="crtionBox relative">
+        <div className="crtionBox relative cursor-pointer ">
         <img src={src} className=" w-full"></img>
         <h1 className=" text-3xl pl-10 max-w-12  text-white font-extralight absolute"> {tital} </h1>
         </div>
