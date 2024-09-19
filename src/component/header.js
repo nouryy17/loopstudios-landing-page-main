e178250078652e336f7c0423a1b8b6edb0b9d051
nouryy17 <dacinour16@gmail.com>
2024-09-19 17:17:31 +0200

    add code

--- src/component/header.js
+++ src/component/header.js
@@ -1,29 +1,27 @@
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
-import {faBars} from "@fortawesome/free-solid-svg-icons"
-import { faXmark} from "@fortawesome/free-solid-svg-icons"
 import img from "../images/logo.svg"
 import "../style/HEADER.sass"
 export default function () {
     return (
-        <header className="  pr-20 pl-20">
+        <header className="  px-10 md:px-20">
           <div className=" flex justify-between  pt-3 pb-3 h-20 items-center "> 
           <img src={img} className=" w-fit"></img>
             <nav className=" flex-1">
-                <ul className=" flex  justify-end ">
+                <ul className=" md:flex  md:djustify-end ">
                       <li className=" lg:mr-16 "><a className=" text-white cursor-pointer ">About</a></li>
-                      <li className=" lg:mr-16 "><a className=" text-white cursor-pointer "> Careers </a></li>
-                      <li className=" lg:mr-16 "><a className=" text-white cursor-pointer "> Events </a></li>
-                      <li className=" lg:mr-16 "><a className=" text-white cursor-pointer "> Products </a></li>
-                      <li className=" lg:mr-16 "><a className=" text-white cursor-pointer "> Support </a></li>
-                </ul> 
+                      <li className=" lg:mr-16 "><a className=" text-white cursor-pointer ">Careers </a></li>
+                      <li className=" lg:mr-16 "><a className=" text-white cursor-pointer ">Events </a></li>
+                      <li className=" lg:mr-16 "><a className=" text-white cursor-pointer ">Products </a></li>
+                      <li className=" lg:mr-16 "><a className=" text-white cursor-pointer ">Support </a></li>
+                </ul>
             </nav>
-            <span className=" md:hidden">
-            <FontAwesomeIcon icon={faBars}  className=" absolute"/>
-            <FontAwesomeIcon icon={faXmark} className=" absolute" />
+            <span className=" md:hidden ">
+            <svg width="20" className=" cursor-pointer activ absolute" height="20" xmlns="http://www.w3.org/2000/svg"><path d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z" fill="#FFF" fill-rule="evenodd"/></svg>
+            <svg width="24" className=" cursor-pointer absolute" height="16" xmlns="http://www.w3.org/2000/svg"><g fill="#FFF" fill-rule="evenodd"><path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z"/></g></svg>
+
             </span>
           </div >
-          <div className=" flex justify-start items-center max-w-xl">
-               <h1 className=" text-white outline p-10 font-bold text-6xl">Immersive experiences that deliver </h1>
+          <div className="title m-auto md:m-0 md:flex md:justify-start items-center md:max-w-lg flex">
+               <h1 className=" text-white font-light  outline p-5  text-5xl uppercase md:text-6xl">Immersive experiences that deliver </h1>
           </div>
         </header>
     )
--- src/component/mainContent.js
+++ src/component/mainContent.js
@@ -8,8 +8,8 @@ export default function () {
         <div className=" md:flex  px-10 py-16  md:px-20 md:py-40 justify-center md:items-center">
             <img src={w ? imgOne : imgPhoneOne} className=" m-auto md:m-0 w-2/3" ></img>
             <div className=" md:w-1/2 w-full md:mt-48  p-16  pb-0 bg-white md:text-sm text-2xl " style={{ transform: w ? "translateX(-130px)" : "translateX(0px)" }}>
-               <h1 className=" md:text-5xl font-semibold text-center uppercase mb-4 md:font-extralight"> The leader in interactive VR</h1>
-               <p className=" text-gray-400 text-center text-xs">  Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
+               <h1 className=" md:text-5xl font-semibold text-center uppercase mb-4 md:text-left md:font-extralight"> The leader in interactive VR</h1>
+               <p className=" text-gray-400 text-center text-xs md:text-left">  Founded in 2011, Loopstudios has been producing world-class virtual reality projects for some of the best companies around the globe. Our award-winning creations have transformed businesses through digital experiences that bind to their brand.</p>
             </div>
         </div>
     )
